17d78495cc9329811e012ce2f37f5531a494b620
jcha-ultra <john@ultratesting.us>
2021-03-24 07:36:37 -0400

    Test: update test state

---
+++ messages/active/message_26_1.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "response": "dependencies_needed",
+    "data": {
+        "dependencies": {
+            "agent_15": "dependency_test_task"
+        }
+    },
+    "msgType": "response",
+    "subtype": "execution",
+    "requestId": "message_23_1",
+    "recipientID": "11_14",
+    "senderId": "0_human",
+    "msgId": "message_26_1"
+}
