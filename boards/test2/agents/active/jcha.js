4edf203786a9c4c200234e47269fec41d589eaa7
jcha-ultra <john@ultratesting.us>
2021-03-26 07:16:48 -0400

    Add static agents

---
+++ boards/test2/agents/active/jcha.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "id": "jcha",
+    "tasks": {
+        "asdfasdf": {
+            "requestId": "message_3_2",
+            "dependencyIds": [],
+            "executionIds": [],
+            "dependentIds": []
+        },
+        "blasphemy": {
+            "requestId": "message_3_3",
+            "dependencyIds": [],
+            "executionIds": [],
+            "dependentIds": []
+        },
+        "task 3": {
+            "requestId": "message_4_4",
+            "dependencyIds": [],
+            "executionIds": [],
+            "dependentIds": []
+        }
+    },
+    "subagents": {
+        "free": [],
+        "busy": []
+    },
+    "subtype": "human"
+}
---
+++ boards/test2/agents/active/source.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "id": "source",
+    "tasks": {
+        "asdfasdf": {
+            "requestId": "message_0_1",
+            "dependencyIds": [],
+            "executionIds": [
+                "message_3_2"
+            ],
+            "dependentIds": []
+        },
+        "blasphemy": {
+            "requestId": "message_1_2",
+            "dependencyIds": [],
+            "executionIds": [
+                "message_3_3"
+            ],
+            "dependentIds": []
+        },
+        "task 3": {
+            "requestId": "message_2_1",
+            "dependencyIds": [],
+            "executionIds": [
+                "message_4_4"
+            ],
+            "dependentIds": []
+        }
+    },
+    "subagents": {
+        "free": [],
+        "busy": []
+    }
+}
