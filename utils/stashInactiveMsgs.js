34b4ea1367e6580d9712b6f58402d7832654f48b
jcha-ultra <john@ultratesting.us>
2021-04-14 09:14:01 -0400

    Stash script: cleanup

--- utils/stashInactiveMsgs.js
+++ utils/stashInactiveMsgs.js
@@ -1,7 +1,3 @@
-
-
-
 const messagePath = `./boards/${process.argv[2]}/messages`;
 const { MessageBoard } = require('../run.js');
-// console.log(messagePath)
 MessageBoard.stashInactiveMsgs(messagePath);
